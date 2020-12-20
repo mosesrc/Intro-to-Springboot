147f81e1aca1f6b6ea4d55d5ee7e0a4b63177449
Ryan Moses <ryanchristophermoses@gmail.com>
2020-12-20 13:21:03 -0500

    First Springboot Application

---
+++ src/test/java/com/tts/hw/MainWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tts.hw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
